120cd0668f8eb6c5087ba838c5eb13eb59fd8cb4
Sehrish <145559546+Sehrish-A4211@users.noreply.github.com>
2024-08-08 13:00:08 +0100

    Add files via upload

---
+++ storage/text.js
@@ -0,0 +1,11 @@
+
+localStorage.setItem('username', 'JohnDoe');
+
+
+let username = localStorage.getItem('username');
+console.log(username); 
+
+
+localStorage.removeItem('username');
+
+localStorage.clear();
